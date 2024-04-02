1f41c3395207a5adda9e01f12033565e296217b7
taira-kobayashi <161178572+taira-kobayashi@users.noreply.github.com>
2024-04-02 12:06:10 +0900

    テストコード　ログイン確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '内容に問題ない場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
