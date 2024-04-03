5ff37988e41e39f242008220f3d8c296d96be59d
taira-kobayashi <161178572+taira-kobayashi@users.noreply.github.com>
2024-04-04 00:28:42 +0900

    テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
-  factory :user do
-    name                  {'test'}
-    last_name             {'バズーカ'}
-    first_name            {'岡田'}
-    last_name_kana        {'バズーカ'}
-    first_name_kana       {'オカダ'}
-    birth                 {Faker::Date.birthday}
-    email                 {Faker::Internet.email}
-    password              {'1a' + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    is_logged_in { true }
 
+  factory :item do
+    association :user
+    item_name             {'testitem'}
+    expanation             {'テストアイテム'}
+    price                 {'1500'}
+    category_id           {'2'}
+    situation_id          {'2'}
+    delivery_id           {'2'}
+    region_id             {'2'}
+    shippingdate_id       {'2'}
 
-    factory :logged_out_user do
-      is_logged_in { false }
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/furima-footer.png'), filename: 'furima-footer.png')
     end
   end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,31 +1,112 @@
 require 'rails_helper'
 
-
-
 RSpec.describe Item, type: :model do
   before do
-    @user = create(:user)
-    @outuser = create(:logged_out_user)
+    @item = FactoryBot.build(:item)
   end
 
-
   describe '商品出品関連機能' do
-    context '内容に問題ない場合' do
-      it '説明をいれる' do
-        expect(@user).to be_valid
+    context '正常系テスト' do
+      it 'ログイン状態で情報を入力してあると出品できる' do
+        expect(@item).to be_valid
       end
+
+      it '出品価格が300円であれば出品できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+
+      it '出品価格が9,999,999円であれば出品できる' do
+        @item.price = 9999999
+        expect(@item).to be_valid
+      end
+
+      it '出品価格が301円から9,999,998円の間であれば出品できる' do
+        @item.price = Faker::Number.between(from: 301, to: 9999998)
+        expect(@item).to be_valid
+      end
+
+
+
+
+
+
     end
 
-    context '内容に問題がある場合' do
-      it '説明をいれる' do
-        @user.name = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include "Name can't be blank"
+    context '異常系テスト' do
+      it '画像データが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+
+      it '商品の説明が空では登録できない' do
+        @item.expanation = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Expanation can't be blank"
+      end
+
+      it 'カテゴリーが選択されていないと登録できない' do
+        @item.category_id = '1' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+
+      it '商品の状態が選択されていないと登録できない' do
+        @item.situation_id = '1' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Situation can't be blank"
+      end
+
+      it '配送料負担情報が選択されていないと登録できない' do
+        @item.delivery_id = '1' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery can't be blank"
+      end
+ 
+      it '発送元の地域が選択されていないと登録できない' do
+        @item.region_id = '1' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Region can't be blank"
+      end
+
+      it '発送までの日数が選択されていないと登録できない' do
+        @item.shippingdate_id = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shippingdate can't be blank"
+      end
+
+      it '料金が入力されていないと登録できない' do
+        @item.price = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+
+      it '出品価格が全角数字だと出品できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+
+      it '出品価格が全角文字だと出品できない' do
+        @item.price = 'あいう'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
       end
 
 
+
+
+
+      it 'が入力されていないと登録できない' do
+        @item.name = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+
+
+    end
+  end
 end
-
-
 
 
