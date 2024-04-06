d7c5a5c01d94e95141e7975399b2c9f812ef389f
taira-kobayashi <161178572+taira-kobayashi@users.noreply.github.com>
2024-04-06 13:28:48 +0900

    画面編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
-  before_action :set_item, only:[:show]
+  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to roth
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,6 +22,23 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+    if current_user.nil?
+      redirect_to new_user_session_path
+    elsif current_user == @item.user
+      render :edit
+    else
+      redirect_to items_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
 private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,11 @@ class Item < ApplicationRecord
   validates :expanation, presence:true
   validates :image, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :delivery_id, presence: true
+  validates :region_id, presence: true
+  validates :shippingdate_id, presence: true
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
