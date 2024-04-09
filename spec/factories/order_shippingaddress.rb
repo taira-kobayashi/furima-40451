e5a5971534f32cdedc28b243c96f95dfa5ff1a88
taira-kobayashi <161178572+taira-kobayashi@users.noreply.github.com>
2024-04-09 14:25:09 +0900

    コードレビュー修正②

--- spec/factories/order_shippingaddress.rb
+++ spec/factories/order_shippingaddress.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     buildingname { "テストビル"}
     tel { "09012345678" }
     token { "tok_abcdefghijk00000000000000000"}
+    region_id             {'2'}
 
   end
 end
--- spec/models/order_shippingaddress_spec.rb
+++ spec/models/order_shippingaddress_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe OrderShippingaddress, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      @test = FactoryBot.build(:order_shippingaddress, user_id: user.id, item_id: item.id, region_id: item.region.id)
+      @test = FactoryBot.build(:order_shippingaddress, user_id: user.id, item_id: item.id)
     end
 
   describe '商品購入機能' do
